b337563f603229bd6da9ad2840820613c096e42b
Ahmed Benamrouche <bbsymphony@bbsymphony-2.local>
2024-11-12 13:34:21 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'oneWay',
+  webDir: 'www'
+};
+
+export default config;
